14678caef4598e10bee03675d022a81b24bd1bfe
Yevhen Horbachov <y.horbachov@gmail.com>
2025-08-24 16:57:22 +0200

    feat: autorun migrations

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: isGoRun,
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
