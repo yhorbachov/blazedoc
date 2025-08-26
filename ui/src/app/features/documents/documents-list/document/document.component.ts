12e9d9bf373eee1c340fe02e55e56488b0fd7220
Yevhen Horbachov <y.horbachov@gmail.com>
2025-08-26 10:53:21 +0200

    feat: layout for documets list

---
+++ ui/src/app/features/documents/documents-list/document/document.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document',
+  template: `Document`,
+})
+export class DocumentComponent {}
--- ui/src/app/features/documents/documents-list/documents-list.component.ts
+++ ui/src/app/features/documents/documents-list/documents-list.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { Document, Folder } from '@core/types';
+import { FolderComponent } from './folder/folder.component';
+import { DocumentComponent } from './document/document.component';
 
 @Component({
   selector: 'app-documents-list',
   templateUrl: './documents-list.component.html',
+  imports: [NgClass, FolderComponent, DocumentComponent],
 })
-export default class DocumentsListComponent {}
+export default class DocumentsListComponent {
+  folders = signal<Folder[]>([
+    {
+      id: 'folder_1',
+      name: 'Folder 1',
+    },
+    {
+      id: 'folder_2',
+      name: 'Folder 2',
+    },
+  ]);
+
+  documents = signal<Document[]>([
+    {
+      id: 'doc_1',
+      name: 'Document 1',
+    },
+    {
+      id: 'doc_2',
+      name: 'Document 2',
+    },
+  ]);
+}
